7ad6d65be3f5905683b60161aad7c95ddc81aa8c
hezhenyang <291062729@qq.com>
2016-07-18 18:25:57 +0800

    update

---
+++ src/main/java/com/xiaoyang/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.xiaoyang.mapper;
+
+import com.xiaoyang.model.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
